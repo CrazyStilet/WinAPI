db35d824bb158d312b65d72340cebe4de81dba54
CrazyStilet <Stilet-2006@yandex.ru>
2015-09-20 23:22:37 +0300

    Рабочая версия

--- Homework 1.1/Homework 1.1/Source.cpp
+++ Homework 1.1/Homework 1.1/Source.cpp
@@ -2,30 +2,37 @@
 #include<tchar.h>
 
 INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpczCmdLine, int nCmdShow) {
-	wchar_t MB1[] = L"Орлов Олег Александрович";
+	TCHAR czName[] = _TEXT("Орлов Олег Александрович");
 	MessageBox(0,
-		MB1,
+		czName,
 		TEXT("ФИО"),
 		MB_OK | MB_ICONINFORMATION);
 	
-	wchar_t MB2[] = L"Ищу работу в сфере информационных технологий";
+	TCHAR czFindWork[] = _TEXT("Ищу работу в сфере информационных технологий");
 	MessageBox(0,
-		MB2,
+		czFindWork,
 		TEXT("Цель"),
 		MB_OK | MB_ICONINFORMATION);
 
-	wchar_t MB3[] = L"Родился 20 июня 1986 года, в городе Николаеве";
+	TCHAR czBorn[] = _TEXT("Родился 20 июня 1986 года, в городе Николаеве");
 	MessageBox(0,
-		MB3,
+		czBorn,
 		TEXT("Общая информация о себе"),
 		MB_OK | MB_ICONINFORMATION);
 	
-	int nSize = wcslen(MB1) / sizeof(wchar_t);
-	wchar_t size[4] = L"101";
+	TCHAR czStudy[] = _TEXT("Учусь в Компьютерной Академии ШАГ на программиста");
+	MessageBox(0,
+		czStudy,
+		TEXT("Учеба"),
+		MB_OK | MB_ICONINFORMATION);
+
+	UINT nSize = (lstrlen(czName) + lstrlen(czFindWork) + lstrlen(czBorn)+lstrlen(czStudy)) / 4;
+	TCHAR buffer[4];
+	wsprintf(buffer, L"%i", nSize);
 
 	MessageBox(0,
-		TEXT("абра-кадабра"),
-		(LPWSTR) nSize,
+		TEXT("В заголовке окна указано среднее число символов в сообщениях\nGoodbye!"),
+		buffer,
 		MB_OK | MB_ICONINFORMATION);
 	return 0;
 }
--- Win32Project1/Win32Project1/Source.cpp
+++
@@ -1,6 +0,0 @@
-#include<windows.h>
-using namespace std;
-
-INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpszCmdLine, int nCmdShow) {
-	MessageBox(0, TEXT("Привет!"), TEXT("Мир!"), MB_OK);
-}
