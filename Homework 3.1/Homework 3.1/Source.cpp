791078945a4f2a099fde769515e4e582fee3bfc5
CrazyStilet <Stilet-2006@yandex.ru>
2015-10-05 20:58:31 +0300

    Проверка не меньше-ли чем 10на10

--- Homework 3.1/Homework 3.1/Source.cpp
+++ Homework 3.1/Homework 3.1/Source.cpp
@@ -1,10 +1,12 @@
 ﻿#include <Windows.h>
 #include "resource.h"
+#include <vector>
 
 BOOL CALLBACK DlgProc(HWND, UINT, WPARAM, LPARAM);
 
 HWND hStatic;
 HINSTANCE hInst;
+
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpszCmdLine, int nCmdShow)
 {
@@ -14,9 +16,11 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpszCmdLine, 
 
 BOOL CALLBACK DlgProc(HWND hWnd, UINT uMessage, WPARAM wParam, LPARAM lParam)
 {
-	static WORD xPosStart, yPosStart,xPosEnd,yPosEnd;
+	static INT xPosStart, yPosStart,xPosEnd,yPosEnd;
 	static int count = 1;
 	TCHAR CountStatic[10];
+	INT Width;
+	INT Height;
 	switch(uMessage)
 	{
 	case WM_CLOSE:
@@ -29,40 +33,45 @@ BOOL CALLBACK DlgProc(HWND hWnd, UINT uMessage, WPARAM wParam, LPARAM lParam)
 	case WM_LBUTTONUP:
 		xPosEnd = LOWORD(lParam);
 		yPosEnd = HIWORD(lParam);
-		/*if((xPosEnd - xPosStart < 10 || yPosEnd - yPosStart < 10) ||
-			(xPosEnd - xPosStart < 10 || yPosStart - yPosEnd > -10) ||
-			(xPosStart - xPosEnd > -10 || yPosEnd - yPosStart < 10) ||
-			(xPosEnd - xPosStart < 10 || yPosStart - yPosEnd > -10))
+
+		Width = xPosEnd - xPosStart;
+		Height = yPosEnd - yPosStart;
+
+		if((Width < 10 && Width > -10) || (Height < 10 && Height > -10))
 		{
 			MessageBox(hWnd, TEXT("Размер Static'a должен быть минимум 10х10"), TEXT("Предупреждение"), MB_OK | MB_ICONINFORMATION);
 			return TRUE;
-		}*/
+		}
+
 		wsprintf(CountStatic, TEXT("Static %i"), count);
 		if(xPosEnd - xPosStart >= 10 && yPosEnd - yPosStart >= 10)
 		{
-			hStatic = CreateWindowEx(0, TEXT("STATIC"), CountStatic, WS_CHILD | WS_VISIBLE | WS_BORDER,
+			hStatic = CreateWindowEx(WS_EX_WINDOWEDGE, TEXT("STATIC"), CountStatic, WS_CHILD | WS_VISIBLE | WS_BORDER,
 				xPosStart, yPosStart, xPosEnd - xPosStart, yPosEnd - yPosStart, hWnd, 0, hInst, 0);
 		}
 		else if(xPosEnd - xPosStart >= 10 && yPosStart - yPosEnd >= 10)
 		{
-			hStatic = CreateWindowEx(0, TEXT("STATIC"), CountStatic, WS_CHILD | WS_VISIBLE | WS_BORDER,
+			hStatic = CreateWindowEx(WS_EX_WINDOWEDGE, TEXT("STATIC"), CountStatic, WS_CHILD | WS_VISIBLE | WS_BORDER,
 				xPosStart, yPosEnd, xPosEnd - xPosStart, yPosStart - yPosEnd, hWnd, 0, hInst, 0);
 		}
 		else if(xPosStart - xPosEnd >= 10 && yPosEnd - yPosStart >= 10)
 		{
-			hStatic = CreateWindowEx(0, TEXT("STATIC"), CountStatic, WS_CHILD | WS_VISIBLE | WS_BORDER,
+			hStatic = CreateWindowEx(WS_EX_WINDOWEDGE, TEXT("STATIC"), CountStatic, WS_CHILD | WS_VISIBLE | WS_BORDER,
 				xPosEnd, yPosStart, xPosStart - xPosEnd, yPosEnd - yPosStart, hWnd, 0, hInst, 0);
 		}
 		else if(xPosStart - xPosEnd >= 10 && yPosStart - yPosEnd >= 10)
 		{
-			hStatic = CreateWindowEx(0, TEXT("STATIC"), CountStatic, WS_CHILD | WS_VISIBLE | WS_BORDER,
+			hStatic = CreateWindowEx(WS_EX_WINDOWEDGE, TEXT("STATIC"), CountStatic, WS_CHILD | WS_VISIBLE | WS_BORDER,
 				xPosEnd, yPosEnd, xPosStart - xPosEnd, yPosStart - yPosEnd, hWnd, 0, hInst, 0);
 		}
-		
+
 		count++;
 		return TRUE;
 	case WM_RBUTTONUP:
 
+		return TRUE;
+	case WM_LBUTTONDBLCLK:
+		MessageBox(hWnd, TEXT("Проверка"), NULL, MB_OK | MB_ICONINFORMATION);
 		return TRUE;
 	}
 	return FALSE;
